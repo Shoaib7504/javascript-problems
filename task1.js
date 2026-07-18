6b263b2deba323657e990dc48fdd77b2dfaa5a48
Md Shoaib Hossain <shoaibhossain188@gmail.com>
2026-07-18 22:19:40 +0600

    js problems

---
+++ task1.js
@@ -0,0 +1,11 @@
+const age = (n) => {
+    if (n >= 18) {
+        return console.log("Your age Adult");
+
+    }
+    else {
+       console.log("You are minor");
+
+    }
+}
+age(188)
---
+++ task2.js
@@ -0,0 +1,42 @@
+// check the number even or odd
+const checkNumber=(n)=>{
+    if(n%2 ==0){
+        return console.log('Your Number is Even');
+        
+    }
+    else{
+        return console.log('Your Number is odd');
+        
+    }
+    
+}
+// checkNumber(45)
+
+// for loop******
+for(let i=1; i<=20; i++){
+    // console.log(i);
+    
+}
+
+// While Loop*****
+let i=1;
+while(i<=15){
+    // console.log(i);
+    i++;
+    
+}
+// Let sum 1 to 20
+let sum=0;
+for(let i=1;i<20;i++){
+    sum +=i;
+}
+// console.log(sum);
+function add(a,b){
+    sum=a+b
+    // console.log(sum);
+    
+}
+add(20,23)
+
+let Numbers=[1,23,434,34534,323,23,345,345,455]
+console.log(Numbers[6]);
